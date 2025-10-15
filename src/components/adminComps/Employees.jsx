4688ebbaf30bd0f54a356c69c3e78a2ed4c812bb
S.Mohamed Sameer <mohamedsameer.s.2007@gmail.com>
2025-10-15 20:15:27 +0530

    Added 'Add Employee' to the admin

--- src/components/adminComps/Employees.jsx
+++ src/components/adminComps/Employees.jsx
@@ -2,7 +2,13 @@ const Employees = () => {
   return (
     <div className="p-4">
       <h2 className="text-xl font-semibold mb-4">Employees</h2>
-      {/* Add your employees content here */}
+      <div className="p-5 w-full rounded bg-gray-100 border border-gray-400 shadow">
+        <h1 className="font-bold ">Add Employee </h1>
+        <div>
+          <input type="text"/>
+          <input type="text"/>
+        </div>
+      </div>
     </div>
   );
 };
